f35059703d28798e1e802fd4180a8661c9117c08
Mohammad Momeni <lo3me@pm.me>
2023-08-19 01:01:52 +0200

    Add docs

--- scripts/main.js
+++ scripts/main.js
@@ -1,40 +1,24 @@
-const delay = 350
+/**
+ * Delay to start calculating since there might be
+ * a lazy load process to load the hours
+ */
+const delay = 500
 
-setTimeout(() => {
-	const monthLabels = Array.from(document.querySelectorAll('svg text[font-size="10"]'));
-	const months = {};
-	let currentMonthLabel = null;
-	monthLabels.forEach(label => {
-		const monthName = label.textContent.trim();
-		months[monthName] = [];
-		currentMonthLabel = monthName;
-	});
-	const elements = document.querySelectorAll('svg text[font-size="10"], svg g[data-toggle="tooltip"][data-original-title]');
-	elements.forEach(element => {
-		const tagName = element.tagName.toLowerCase();
-		if (tagName === 'text') {
-			const monthName = element.textContent.trim();
-			currentMonthLabel = monthName;
-		} else if (tagName === 'g') {
-			if (currentMonthLabel !== null) {
-				months[currentMonthLabel].push(element);
-			}
-		}
-	});
-	const monthSums = {};
-	for (const monthName in months) {
-		monthSums[monthName] = 0;
-		months[monthName].forEach(element => {
-			const title = element.getAttribute('data-original-title');
-			const time = parseFloat(title.replace("h", "."));
-			if (!isNaN(time)) {
-				monthSums[monthName] += Math.floor(time);
-				monthSums[monthName] += (time - Math.floor(time)) / 6 * 10;
-			}
-		});
-		monthSums[monthName] = (monthSums[monthName] + ((monthSums[monthName] - Math.floor(monthSums[monthName])) * 0.6)).toFixed(1);
-	}
+/**
+ * Calculates the sum of hours of each month
+ * @param {HTMLElement[]} monthLabels
+ * @returns {Object}
+ */
+function calcHours(monthLabels) {
 
+}
+
+/**
+ * Shows the calculated hours below each paired month label
+ * @param {HTMLElement[]} monthLabels
+ * @param {Object} monthSums
+ */
+function displayHours(monthLabels, monthSums) {
 	monthLabels.forEach(label => {
 		const hour = label.cloneNode(true)
 		hour.setAttribute("y", (parseInt(hour.getAttribute("y")) + 12).toString())
@@ -44,4 +28,15 @@ setTimeout(() => {
 		hour.textContent = monthSums[label.textContent.trim()] + "h"
 		label.parentNode.insertBefore(hour, label.nextSibling);
 	})
-}, delay);
+}
+
+/**
+ * Main task representor
+ */
+function task() {
+	const monthLabels = Array.from(document.querySelectorAll('svg text[font-size="10"]'));
+	const monthSums = calcHours(monthLabels);
+	displayHours(monthLabels, monthSums);
+}
+
+setTimeout(task, delay);
