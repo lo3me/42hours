713fe36c15bb74cf9bffea260ba482aff82792c7
Azer Sioud <sioudazer8@gmail.com>
2023-08-19 04:44:05 +0200

    Update main.js
    Added Extension's back-end

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,42 @@ const delay = 400
  * @returns {Object}
  */
 function calcHours(monthLabels) {
+	const months = {};
+	let currentMonthLabel = null;
 
+	monthLabels.forEach(label => {
+		const monthName = label.textContent.trim();
+		months[monthName] = [];
+		currentMonthLabel = monthName;
+	});
+
+	const elements = document.querySelectorAll('svg text[font-size="10"], svg g[data-toggle="tooltip"][data-original-title]');
+	elements.forEach(element => {
+		const tagName = element.tagName.toLowerCase();
+		if (tagName === 'text') {
+			const monthName = element.textContent.trim();
+			currentMonthLabel = monthName;
+		} else if (tagName === 'g') {
+			if (currentMonthLabel !== null) {
+			months[currentMonthLabel].push(element);
+			}
+		}
+	});
+
+	const monthSums = {};
+	for (const monthName in months) {
+		monthSums[monthName] = 0;
+		months[monthName].forEach(element => {
+			const title = element.getAttribute('data-original-title');
+			const time = parseFloat(title.replace("h", "."));
+			if (!isNaN(time)) {
+				monthSums[monthName] += Math.floor(time);
+				monthSums[monthName] += (time - Math.floor(time)) / 6 * 10;
+			}
+		});
+		monthSums[monthName] = (monthSums[monthName] + ((monthSums[monthName] - Math.floor(monthSums[monthName])) * 0.6)).toFixed(2);
+	}
+	return monthSums;
 }
 
 /**
